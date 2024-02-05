f793b679ff0f42ff480dfcd65fb32d6a70297870
tinho <jhonbest170@gmail.com>
2024-02-04 22:59:53 -0300

    update

---
+++ reveal.js
@@ -0,0 +1,67 @@
+window.animation=ScrollReveal({reset:true})
+
+animation.reveal('.direito',{
+  duration:800,
+  distance:"90px",
+  delay:200,
+})
+animation.reveal('.img', {
+  duration:1000,
+  distance: "90px",
+  delay:400,
+})
+
+animation.reveal('.div-card1', {
+  duration: 1000,
+  distance: "90px",
+  delay: 400,
+})
+animation.reveal('.div-card2', {
+  duration: 1000,
+  distance: "90px",
+  delay:600,
+})
+animation.reveal('.div-card3', {
+  duration:1000,
+  distance: "90px",
+  delay:700,
+})
+
+animation.reveal('.card', {
+  origin:"left"
+})
+animation.reveal('.txt', {
+  duration:1000,
+  distance:"90px",
+  origin:"left",
+  delay:200
+})
+animation.reveal('.projeto-origin',{
+  duration: 1000,
+   distance:"90px",
+   origin: "right"
+})
+animation.reveal('.pt-1',{
+  origin:"left",
+  delay:100
+})
+animation.reveal('.pt-2', {
+  origin:"right",
+  delay:200
+})
+animation.reveal('.pt-3', {
+  origin:"left",
+  delay:300
+})
+animation.reveal('.pessoaImg', {
+  duration:1000,
+  distance:"90px",
+  origin: "left",
+  delay:200
+})
+animation.reveal('.sobre', {
+  duration:1000,
+  distance:"90px",
+  origin: "right",
+  delay: 300
+})
